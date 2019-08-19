dc8bd2f4044bacd52843898cf1dad99c52ff8909
Javier Pereyra <javier.pereyra183@gmail.com>
2019-08-19 02:06:01 -0300

    initial commit

---
+++ practica6/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace practica6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.chkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rBtn1 = new System.Windows.Forms.RadioButton();
+            this.rBtn2 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(111, 13);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(534, 22);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(672, 11);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(102, 48);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // chkBox1
+            // 
+            this.chkBox1.AutoSize = true;
+            this.chkBox1.Location = new System.Drawing.Point(132, 93);
+            this.chkBox1.Name = "chkBox1";
+            this.chkBox1.Size = new System.Drawing.Size(18, 17);
+            this.chkBox1.TabIndex = 3;
+            this.chkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Programador";
+            // 
+            // rBtn1
+            // 
+            this.rBtn1.AutoSize = true;
+            this.rBtn1.Location = new System.Drawing.Point(121, 48);
+            this.rBtn1.Name = "rBtn1";
+            this.rBtn1.Size = new System.Drawing.Size(17, 16);
+            this.rBtn1.TabIndex = 5;
+            this.rBtn1.TabStop = true;
+            this.rBtn1.UseVisualStyleBackColor = true;
+            this.rBtn1.CheckedChanged += new System.EventHandler(this.rBtn1_CheckedChanged);
+            // 
+            // rBtn2
+            // 
+            this.rBtn2.AutoSize = true;
+            this.rBtn2.Location = new System.Drawing.Point(417, 48);
+            this.rBtn2.Name = "rBtn2";
+            this.rBtn2.Size = new System.Drawing.Size(17, 16);
+            this.rBtn2.TabIndex = 6;
+            this.rBtn2.TabStop = true;
+            this.rBtn2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "HOMBRE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(338, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "MUJER";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Salida";
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(34, 305);
+            this.txtBox2.Multiline = true;
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(703, 133);
+            this.txtBox2.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rBtn2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.rBtn1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(95, 147);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(579, 100);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chkBox1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.CheckBox chkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rBtn1;
+        private System.Windows.Forms.RadioButton rBtn2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ practica6/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Deshabilito el boton ok.
+            btnOK.Enabled = false;
+        }
+
+        private void ValidarOk()
+        {
+            //Habilitar el boton, Siempre y cuando txtNombre tenga datos.
+            btnOK.Enabled = (txtNombre.BackColor != Color.Red);
+           
+        }
+
+        private void txtNombre_Validating(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+
+            if(tb.Text.Length == 0)
+            {
+                tb.BackColor = Color.Red;
+            }
+            else
+            {
+                tb.BackColor = System.Drawing.SystemColors.Window;
+                ValidarOk();
+            }
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            //No valido datos que si el Boton esta habilitado.
+            //es por que paso el Evento Validating del Nombre.
+            String salida; // Declaro una variable string para resguardar
+                           // la salida
+
+            salida = "Nombre: " + txtNombre.Text + "\r\n";
+            salida += "Ocupacion: " + (string)(chkBox1.Checked ? "Programador" : "No es programador") + "\r\n";
+            salida += "Sexo: " + (string)(rBtn1.Checked ? "Hombre" : "Mujer") + "\r\n";
+
+            //Vuelco la salida al Textbox Salida.
+            txtBox2.Text = salida;
+
+        }
+
+        private void rBtn1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
